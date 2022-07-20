c39e0120dc3ad9300ee7688d98f415833684a340
giuliobonacci <giubonacci@outlook.it>
2022-07-20 09:14:12 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { BeerListComponent } from './components/beer-list/beer-list.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BeerFormComponent } from './components/beer-form/beer-form.component';
+import { BeerService } from './services/beer.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [BeerService],
   declarations: [AppComponent, BeerListComponent, BeerFormComponent],
   bootstrap: [AppComponent],
 })
