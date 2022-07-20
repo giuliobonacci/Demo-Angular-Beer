99abacdcaa54df6021e65262725be866d926b64e
giuliobonacci <giubonacci@outlook.it>
2022-07-20 12:06:35 +0200

    3 files modified

--- src/app/components/beer-form/beer-form.component.ts
+++ src/app/components/beer-form/beer-form.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { allBeerTypes, BeerForm } from '../../model/model';
 
 @Component({
   selector: 'app-beer-form',
   templateUrl: './beer-form.component.html',
-  styleUrls: ['./beer-form.component.css']
+  styleUrls: ['./beer-form.component.css'],
 })
 export class BeerFormComponent implements OnInit {
+  beerTypes = allBeerTypes;
+  beerForm: FormGroup<BeerForm>;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.beerForm = this.initForm();
   }
-
+  private initForm(): FormGroup<BeerForm> {
+    return new FormGroup<BeerForm>({
+      beerName: new FormControl(null, Validators.required),
+      beerStyle: new FormControl(null, Validators.required),
+      upc: new FormControl(null, Validators.required),
+      price: new FormControl(null, Validators.required),
+    });
+  }
 }
--- src/app/model/model.ts
+++ src/app/model/model.ts
@@ -1,3 +1,5 @@
+import { Form, FormControl } from '@angular/forms';
+
 export type BeerStyle =
   | 'ALE'
   | 'GOSE'
@@ -10,6 +12,21 @@ export type BeerStyle =
   | 'STOUT'
   | 'HEAT'
   | 'WHEAT';
+
+export const allBeerTypes: BeerStyle[] = [
+  'ALE',
+  'GOSE',
+  'IPA',
+  'LAGER',
+  'PALE_ALE',
+  'PILSNER',
+  'PORTER',
+  'SAISON',
+  'STOUT',
+  'HEAT',
+  'WHEAT',
+];
+
 export interface Beer {
   id: number;
   beerName: string;
@@ -18,4 +35,11 @@ export interface Beer {
   price: number;
   createdDate: Date;
   lastModifiedDate: Date;
+}
+
+export interface BeerForm {
+  beerName: FormControl<string | undefined | null>;
+  beerStyle: FormControl<BeerStyle | undefined | null>;
+  upc: FormControl<string | undefined | null>;
+  price: FormControl<number | undefined | null>;
 }
